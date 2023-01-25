12e891266ee2ed339f47be511a880f2a784dd9b2
Nikita Usov <nikitariusov@gmail.com>
2023-01-26 01:23:48 +0200

    feat: end task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  console.log(str);
+  let arr = bracketsConfig.map(elem => {
+    return elem.join('')
+  })
+  console.log(arr);
+
+
+  for (let i = 0; i < arr.length; i++) {
+    if (str.includes(arr[i])) {
+      console.log('Строка до: ', str)
+      str = str.replaceAll(arr[i], '')
+      console.log('Строка после: ', str);
+      i = -1
+    }
+  }
+
+  if (str.length === 0) {
+    return true
+  } else {
+    return false
+  }
 }
